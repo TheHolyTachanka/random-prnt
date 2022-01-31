4fd0bb0517ce54327b43e1244487f3eb78f98116
TheHolyTachanka <92385974+TheHolyTachanka@users.noreply.github.com>
2022-01-31 06:36:07 +0200

    lol

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import webbrowser
+
+
+
+letters1 = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+letters2 = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+numbers1 = "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+numbers2 = "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+numbers3 = "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+numbers4 = "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+
+url = print("https://prnt.sc/" + random.choice(letters1) + random.choice(letters2) + random.choice(numbers1) + random.choice(numbers2) + random.choice(numbers3) + random.choice(numbers4))
+
